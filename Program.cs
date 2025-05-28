980df0f9eb7b660c60763c979df24dd3d5755ea7
waylo <waylo@LAPTOP-79O7L82S>
2025-05-28 13:03:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string json = File.ReadAllText("testdata.txt");
+
+        JObject obj = JObject.Parse(json);
+        JArray organicResults = (JArray)obj["organic_results"];
+
+        foreach (var result in organicResults)
+        {
+            string title = result["title"]?.ToString();
+            string snippet = result["snippet"]?.ToString();
+
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Snippet: {snippet}");
+            Console.WriteLine();
+
+            Console.WriteLine("data exists now");
+        }
+    }
+}
