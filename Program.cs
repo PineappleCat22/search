0c4b5521c5992d68ad750eb39d19df7d7c9779f3
PineappleCat22 <waylonmonroe418@gmail.com>
2025-05-28 19:13:56 -0700

    parsing data complete. now query

--- Program.cs
+++ Program.cs
@@ -5,21 +5,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        string json = File.ReadAllText("testdata.txt");
+        //query the api here
+        string json = File.ReadAllText(@"C:\Users\waylo\source\repos\PineappleCat22\search\testdata2.txt");
 
         JObject obj = JObject.Parse(json);
         JArray organicResults = (JArray)obj["organic_results"];
+        JArray relatedQuestions = (JArray)obj["related_questions"];
 
+        int resultCounter = 0;
         foreach (var result in organicResults)
         {
-            string title = result["title"]?.ToString();
-            string snippet = result["snippet"]?.ToString();
-
-            Console.WriteLine($"Title: {title}");
-            Console.WriteLine($"Snippet: {snippet}");
+            resultCounter++;
             Console.WriteLine();
-
-            Console.WriteLine("data exists now");
+            Console.WriteLine("Result #" + resultCounter);
+            Console.WriteLine($"Title: {result["title"]?.ToString()}");
+            Console.WriteLine($"Snippet: {result["snippet"]?.ToString()}");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to display the next result, or press q to quit.");
+            if (Console.ReadKey().Key == ConsoleKey.Q)
+            {
+                System.Environment.Exit(0);
+            }
+            if (resultCounter == 10)
+            {
+                Console.WriteLine("That was the last result. Lets hope what you're looking for is in the Related Questions.");
+                break;
+            }
+        }
+        resultCounter = 0;
+        foreach (var question in relatedQuestions)
+        {
+            resultCounter++;
+            Console.WriteLine();
+            Console.WriteLine("Result #" + resultCounter);
+            Console.WriteLine($"Snippet: {question["snippet"]?.ToString()}");
+            Console.WriteLine();
+            if (resultCounter == 4)
+            {
+                Console.WriteLine("That was the last result. Exiting...");
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine("Press any key to display the next result, or press q to quit.");
+            if (Console.ReadKey().Key == ConsoleKey.Q)
+            {
+                System.Environment.Exit(0);
+            }
         }
     }
 }
